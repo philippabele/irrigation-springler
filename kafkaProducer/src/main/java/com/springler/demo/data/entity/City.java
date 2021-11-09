0eff9ed4ff222bd1db49cdda2c1159f8f336a7b8
Philipp Abele <54260843+philippabele@users.noreply.github.com>
2021-11-09 12:24:18 +0100

    add city

---
+++ kafkaProducer/src/main/java/com/springler/demo/data/entity/City.java
@@ -0,0 +1,18 @@
+package com.springler.demo.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@NonNull
+public class City {
+
+    private String name;
+    private Double lat;
+    private Double lon;
+
+}
