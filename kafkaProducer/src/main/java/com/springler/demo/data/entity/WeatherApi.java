4278e67491bf96868d58f804ca3f1654f300cdec
Philipp Abele <54260843+philippabele@users.noreply.github.com>
2021-11-09 12:49:12 +0100

    add url builder

---
+++ kafkaProducer/src/main/java/com/springler/demo/data/entity/WeatherApi.java
@@ -0,0 +1,22 @@
+package com.springler.demo.data.entity;
+
+import java.util.StringJoiner;
+
+public class WeatherApi {
+
+    public static History getHistory(City city, long time, String apikey) {
+
+        StringJoiner urlJoiner = new StringJoiner("");
+
+        urlJoiner.add("http://api.openweathermap.org/data/2.5/onecall/timemachine");
+        urlJoiner.add("?lat=" + city.getLat().toString());
+        urlJoiner.add("&lon=" + city.getLon().toString());
+        urlJoiner.add("&dt=" + Long.toString(time));
+        urlJoiner.add("&appid=" + apikey);
+
+        History history = new History();
+
+        return history;
+    }
+
+}
