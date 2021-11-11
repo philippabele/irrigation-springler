4200a9f69190cb2bf24bf7ff2375b62e219b7ad6
Philipp Abele <54260843+philippabele@users.noreply.github.com>
2021-11-11 01:00:37 +0100

    add filter and map to stream

--- kafkaStreamer/src/main/java/com/springler/demo/DemoStreamer.java
+++ kafkaStreamer/src/main/java/com/springler/demo/DemoStreamer.java
@@ -1,5 +1,6 @@
 package com.springler.demo;
 
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +9,20 @@ import java.util.function.Function;
 
 import com.springler.demo.data.entity.History;
 import com.springler.demo.data.entity.Hourly;
+import com.springler.demo.data.entity.PrecipitationDataPoint;
 
 @Configuration
 public class DemoStreamer {
 
+  private static PrecipitationDataPoint formatHourly(Hourly hourly) {
+    PrecipitationDataPoint dataPoint = new PrecipitationDataPoint(hourly);
+    return dataPoint;
+  }
+
   @Bean
-  public Function<KStream<String, History>, KStream<String, Hourly>> streamApp() {
+  public Function<KStream<String, History>, KStream<String, PrecipitationDataPoint>> streamApp() {
 
-    return kstream -> kstream.flatMapValues(history -> history.getHourly());
-
+    return kstream -> kstream.flatMapValues(history -> history.getHourly())
+        .filter((key, hour) -> hour.getRain() != null).map((key, hourly) -> new KeyValue<>(key, formatHourly(hourly)));
   }
 }
