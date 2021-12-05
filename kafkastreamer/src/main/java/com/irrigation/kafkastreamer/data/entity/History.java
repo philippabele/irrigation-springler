b74abb9b526f2240133ff9bb8cb99ca083114027
Philipp Abele <54260843+philippabele@users.noreply.github.com>
2021-12-06 00:35:12 +0100

    add business logik

---
+++ kafkastreamer/src/main/java/com/irrigation/kafkastreamer/data/entity/History.java
@@ -0,0 +1,42 @@
+
+package com.irrigation.kafkastreamer.data.entity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "lat", "lon", "timezone", "timezone_offset", "current", "hourly" })
+@Generated("jsonschema2pojo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@NonNull
+public class History {
+
+    @JsonProperty("lat")
+    private Double lat;
+    @JsonProperty("lon")
+    private Double lon;
+    @JsonProperty("timezone")
+    private String timezone;
+    @JsonProperty("timezone_offset")
+    private Integer timezoneOffset;
+    @JsonProperty("current")
+    private Current current;
+    @JsonProperty("hourly")
+    private List<Hourly> hourly = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+}
---
+++ kafkastreamer/src/main/java/com/irrigation/kafkastreamer/data/entity/Rain.java
@@ -0,0 +1,31 @@
+
+package com.irrigation.kafkastreamer.data.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "1h" })
+@Generated("jsonschema2pojo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@NonNull
+public class Rain {
+
+    @JsonProperty("1h")
+    private Double _1h;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+}
